b141aa55c7f500a40a966e4531b34bade0248d0d
matricaira <matricaira@gmail.com>
2025-03-26 18:04:02 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // swcMinify sudah default di Next.js 15, jadi nggak perlu
-  experimental: {
-    appDir: true, // Sesuai dengan struktur Next.js terbaru
-  },
 };
 
 export default nextConfig;
