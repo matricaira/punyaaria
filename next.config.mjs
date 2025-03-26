668803d9fea224373b88280b6a3843b9f722681d
Aria Aura Rachman <ariaaurar@gmail.com>
2025-03-26 17:23:58 +0700

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Add any other necessary configuration here
+};
+
+export default nextConfig;
+
